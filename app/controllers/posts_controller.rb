091d47c194cd2ee329bbd415564f561e3e0ea260
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2019-06-04 17:44:21 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,12 +7,27 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
   def update
     @post = Post.find(params[:id])
-
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if Post.new(post_params).valid?
+      @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
